08c9509bfa8706c35698fc5c993d564f429631c3
chrvnixx <emmanueladekola91@gmail.com>
2026-08-04 21:13:11 +0100

    data transfer objects

---
+++ src/main/java/com/app/ecom_application/dto/UserRequest.java
@@ -0,0 +1,13 @@
+package com.app.ecom_application.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRequest {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private AddressDto address;
+}
